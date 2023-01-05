a3b64571cf7030637f0b7a3fb943d71c10e93848
han20230105 <121998047+han20230105@users.noreply.github.com>
2023-01-05 21:10:33 +0800

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	//int a = 0;
+	//int b = 2;
+	//if (a == 1)
+	//{
+		//if (b == 2)
+			//printf("hehe\n");
+	//}
+		//else
+			//printf("haha\n");
+			//int num = 4;
+			//if (5 == num)
+				//printf("Ð»Ð»\n");
+	//int a = 1;
+	//while (a <= 100)
+	//{
+		//if (a % 2 == 0)//%ÊÇÈ¡Óà
+			//printf("%d ", a);
+		//a++;
+	//}
+	//int a = 0;
+	//scanf("%d", &a);
+	//switch (a)
+	//{
+	//case 1:
+		//printf("one");
+		//if (a == 1)
+			//printf("hehe");
+		//break;
+	//case 2:
+		//printf("two");
+		//break;
+	//default:
+		//printf("ÊäÈë´íÎó");
+		//break;
+	
+	//}
+	/*int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:
+		m++;
+	case 2:
+		n++;
+	case 3:
+		switch (n)
+		{
+		case 1:
+			n++;
+		case 2:
+			m++;
+			n++;
+			break;
+		}
+	case 4:
+		m++;
+		break;
+	default:
+		break;
+
+	}
+	printf("n =%d\n m =%d\n", n, m);*/
+	/*int a = 1;
+	while (a <= 10)
+	{
+		if (a == 5)
+			continue;
+		printf("%d ", a);
+		a++;
+	}*/
+	/*int i = 0;
+	while ((i = getchar()), i !=EOF)
+		putchar(i)*/
+
+	return 0;
+}
