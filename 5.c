5672400f5f12e0b72bd9418c931388ccf504d910
han20230105 <121998047+han20230105@users.noreply.github.com>
2023-01-07 21:35:06 +0800

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int i = 0;
+	//for (i = 0; i < 10; i++)//前闭后开区间
+	//{
+	//	printf("%d ", arr[i]);
+	//}
+	/*int i = 0;
+	int j = 0;
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			printf("hehe\n");
+		}
+	/*}*/
+	/*int x, y;
+	for (x = 0, y = 0; x < 2 && y < 5; ++x, y++)
+	{
+		printf("haha\n");
+	}*/
+	int a = 0;
+	int b = 0;
+		for (a = 0, b = 0; b = 0; a++, ++b)
+			++b;
+	return 0;
+}
