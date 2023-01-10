f2a7b33660e9a5e46a73cb1537d07588b3f62306
han20230105 <121998047+han20230105@users.noreply.github.com>
2023-01-10 21:07:17 +0800

    Add files via upload

--- 日常.c
+++ 日常.c
@@ -168,3 +168,61 @@
 //	printf("最大的数为：%d", max);
 //	return 0;//10个字符求最大值；
 //}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//		for (a = 1; a < 10; a++)
+//		{
+//		
+//			for (b = 1; b <= a; b++)
+//			{
+//				c = a * b;
+//				printf("%d*%d=%-2d ", a, b, c);//2d是打印二位（-2d）是左对齐，没有打印空格，保证对齐
+//			}
+//			printf("\n");//换行之关键；
+//		}
+//	return 0;
+//}//乘法口诀表；
+
+//int main()
+//{
+//	int a = 0;
+//	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//	int sz = sizeof(arr)/sizeof(arr[0]);
+//	int k = 7;
+//	int left = 0;
+//	int right = sz - 1;
+//	while(left<=right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//	
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//
+//		}
+//		else 
+//		 {
+//			printf("找到了且下标等于%d", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//		printf("未找到");
+//
+//	
+//	return 0;
+//}//二分查找法
+ 
+int main()
+{
+
+	return 0;
+}
